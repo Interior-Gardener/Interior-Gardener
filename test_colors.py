5dadd6db46f214698bad922a342d91873d67dff9
Kartik Verma <kartikverma2204number1@gmail.com>
2026-08-08 19:39:32 +0530

    Final commit to temp hopefully

--- test_colors.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open('assets/portrait_svg.svg', 'r') as f:
-    content = f.read()
-fills = re.findall(r'fill=\"([^\"]+)\"', content)
-print('Number of unique colors:', len(set(fills)))
-print('Sample colors:', list(set(fills))[:10])
